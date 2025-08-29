9eee626beb5adf1d72ef178823e95f3aaa016911
Felipe <paezrinconfelipe7@gmail.com>
2025-08-29 00:18:43 -0500

    1.0 Se crea una mejora en el catalogo, donde damos una pequeña info y lo mas importante se crea un boton que redirige a whatsapp directamente en cada img, esto conlleva js y html

--- js/funciones_hombre.js
+++ js/funciones_hombre.js
@@ -6,3 +6,10 @@ function mostrarUniforme(src) {
 function cerrarVista() {
   document.getElementById("vista-uniforme").style.display = "none";
 }
+function enviarWhatsApp(imagenURL){
+  const mensaje = encodeURIComponent( //Codifica el mensaje para que sea válido en una URL
+    //window.location.origin obtiene el dominio actual (por ejemplo: https://uniformesclau.com)
+    `Hola, estoy interesado en este uniforme: \n${window.location.origin}${imagenURL}`
+  );
+  window.open(`https://wa.me/573203341083?text=${mensaje}`, "_blank")
+}
